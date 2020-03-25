9f1d452fd61e0b71fdbae2589b1360dc26175694
SkyHeads <skyheads@live.com>
2020-03-25 02:26:27 -0300

    Add Pagination on IncidentController

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,12 @@ import connection from '../database';
 
 export default {
   async index(req, res) {
-    const incidents = await connection('incidents').select('*');
+    const { page = 1 } = req.query;
+
+    const incidents = await connection('incidents')
+      .select('*')
+      .limit(5)
+      .offset((page - 1) * 5);
 
     return res.json(incidents);
   },
