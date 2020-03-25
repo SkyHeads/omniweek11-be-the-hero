2b688d28945636a398c3f723b8c9ed15e807fb5c
SkyHeads <skyheads@live.com>
2020-03-25 00:35:40 -0300

    Create connection to sqlite and route.post/ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,26 @@
 import { Router } from 'express';
+import crypto from 'crypto';
+import connection from './database';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ ok: true });
+routes.post('/ongs', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return res.json({
+    id,
+  });
 });
 
 export default routes;
